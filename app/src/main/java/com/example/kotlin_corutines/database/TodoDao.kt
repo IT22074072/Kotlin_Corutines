d503070e52249e3ab62c82b24adf2bdcb157284e
IT22074072 <it22074072@my.sliit.lk>
2024-05-20 15:56:29 +0530

    TodoDao

---
+++ app/src/main/java/com/example/kotlin_corutines/database/TodoDao.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin_corutines.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+
+    @Insert
+    suspend fun insertTodo(todo: Todo)
+
+    @Delete
+    suspend fun  deleteTodo(todo: Todo)
+
+    @Query("SELECT * FROM Todo")
+    fun getAllTodoItems():List<Todo>
+}
