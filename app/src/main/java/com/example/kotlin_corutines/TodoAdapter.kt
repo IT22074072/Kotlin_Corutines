f9881a791628375e80272d239db932005060dd1c
IT22074072 <it22074072@my.sliit.lk>
2024-05-20 17:29:29 +0530

    Update Adapter-Update class constructor parameters and initialized the variables

--- app/src/main/java/com/example/kotlin_corutines/TodoAdapter.kt
+++ app/src/main/java/com/example/kotlin_corutines/TodoAdapter.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlin_corutines.database.Todo
+import com.example.kotlin_corutines.database.TotoRepository
 
-class TodoAdapter:RecyclerView.Adapter<ToDoViewHolder>(){
+class TodoAdapter(items:List<Todo>, repository: TotoRepository, viewModel: MainActivityData):RecyclerView.Adapter<ToDoViewHolder>(){
 
-    private lateinit var context: Context
+    var context: Context? = null
+    val items = items
+    val repository = repository
+    val viewModel = viewModel
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.view_item, parent, false)
 
