ba9307894d9dcd98d556d0e5a32cd3eb1577246d
IT22074072 <it22074072@my.sliit.lk>
2024-05-20 10:29:28 +0530

    Coroutine Example

--- app/src/main/java/com/example/kotlin_corutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_corutines/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.kotlin_corutines
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +23,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textView:TextView = findViewById(R.id.textView)
+        val button:Button = findViewById(R.id.button)
+
+
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            counter(textView)
+        }
+
+
+        button.setOnClickListener(){
+            Toast.makeText(this, "This is a button click", Toast.LENGTH_LONG).show()
+            runBlocking {
+                delay(3000)
+            }
+        }
+    }
+
+
+    //updating a ui component(with the suspend function)
+
+    //if you want to run something parallel to the normal process
+    private suspend fun counter(view:TextView){
+        var x = 0
+        while (true){
+            view.text = x.toString()
+            delay(1000)
+            x++
         }
     }
 }
